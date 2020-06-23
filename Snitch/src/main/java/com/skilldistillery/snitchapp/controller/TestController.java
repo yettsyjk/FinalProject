3e5570d413d435bca2dc09eb8e1c810d34f23692
Iryna <kefa_tretinik@icloud.com>
2020-06-23 14:11:50 -0600

    CrossOrigin

--- Snitch/src/main/java/com/skilldistillery/snitchapp/controller/TestController.java
+++ Snitch/src/main/java/com/skilldistillery/snitchapp/controller/TestController.java
@@ -1,12 +1,13 @@
 package com.skilldistillery.snitchapp.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
-
+@CrossOrigin({"*", "http://localhost:4210"})
 public class TestController {
 
 	@GetMapping("ping")
