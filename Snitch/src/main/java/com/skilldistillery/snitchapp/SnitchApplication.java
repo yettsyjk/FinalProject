e401c9e5b83a0b4e7f9464cd8213e49af13d2099
Yettsy Knapp <yettsyknapp@SDMacBookPro3-2.local>
2020-06-23 11:45:37 -0600

    setup Snitch Spring Starter Project

---
+++ Snitch/src/main/java/com/skilldistillery/snitchapp/SnitchApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.snitchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnitchApplication.class, args);
+	}
+
+}
---
+++ Snitch/src/test/java/com/skilldistillery/snitchapp/SnitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.snitchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
