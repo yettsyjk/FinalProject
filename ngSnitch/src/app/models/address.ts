a57a7323190db668e637032593016647eef74f5d
stevie-steve <sharpsrjr@gmail.com>
2020-06-29 16:00:24 -0600

    models for Address, comment and snitch_vote

--- ngSnitch/src/app/models/address.ts
+++ ngSnitch/src/app/models/address.ts
@@ -1,2 +1,24 @@
 export class Address {
+  id: number;
+  pinName: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: number;
+
+  constructor(
+    id?: number,
+    pinName?: string,
+    street?: string,
+    city?: string,
+    state?: string,
+    zip?: number) {
+
+      this.id = id;
+      this.pinName = pinName;
+      this.street = street;
+      this.city = city;
+      this.state = state;
+      this.zip = zip;
+  }
 }
--- ngSnitch/src/app/models/snitch-vote.ts
+++ ngSnitch/src/app/models/snitch-vote.ts
@@ -1,2 +1,25 @@
+import { User } from './user';
+import { Snitch } from './snitch';
+
 export class SnitchVote {
+  vote: boolean;
+  createTime: string;
+  note: string;
+  snitch: Snitch;
+  user: User;
+
+  constructor(
+    vote?: boolean,
+    createTime?: string,
+    note?: string,
+    snitch?: Snitch,
+    user?: User) {
+
+      this.vote = vote;
+      this.createTime = createTime;
+      this.note = note;
+      this.snitch = snitch;
+      this.user = user;
+    }
 }
+
