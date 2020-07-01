2608a407c1bcd233d6239b5d859b1bcf67decfc4
stevie-steve <sharpsrjr@gmail.com>
2020-07-01 15:34:26 -0600

    snitchcount pipe

--- ngSnitch/src/app/components/home/home.component.ts
+++ ngSnitch/src/app/components/home/home.component.ts
@@ -1,39 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbAccordionConfig} from '@ng-bootstrap/ng-bootstrap';
+import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
 import { SnitchService } from 'src/app/services/snitch.service';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-
+import { SnitchCountPipe } from 'src/app/pipes/snitch-count.pipe';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  snitches = [];
 
-
-  constructor(config: NgbAccordionConfig,
-              private snitchService: SnitchService,
-              private router: Router,
-              private authService: AuthService ) {
+  constructor(
+    config: NgbAccordionConfig,
+    private snitchService: SnitchService,
+    private router: Router,
+    private authService: AuthService,
+    private snitchCount: SnitchCountPipe
+  ) {
     config.closeOthers = true;
     config.type = ''; // changes color of accordion background
     // config.type.bold=;
   }
 
   ngOnInit(): void {
+    this.loadSnitches();
   }
-//this.router.navigateByUrl('/snitches');
+  //this.router.navigateByUrl('/snitches');
 
-checkLogin(){
-  return this.authService.checkLogin();
+  checkLogin() {
+    return this.authService.checkLogin();
+  }
+
+  loadSnitches() {
+    this.snitchService.findAll().subscribe(
+      (snitches) => {
+        console.log(snitches);
+        snitches.reverse();
+        this.snitches = snitches;
+      },
+      (noGo) => {
+        console.error('SnitchListComponent.index(): error retrieving snitches');
+        console.error(noGo);
+      }
+    );
+  }
+
+  getNumberOfAllSnitches() {
+    return this.snitchCount.transform(this.snitches).length;
+  }
 }
-
-  }
-
-
-
-
-
--- ngSnitch/src/app/pipes/snitch-count.pipe.ts
+++ ngSnitch/src/app/pipes/snitch-count.pipe.ts
@@ -1,12 +1,37 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Snitch } from '../models/snitch';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+import { SnitchService } from '../services/snitch.service';
 
 @Pipe({
   name: 'snitchCount'
 })
 export class SnitchCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+transform(snitches: Snitch[]): Snitch[] {
+  const results = [];
 
+  for (let i = 0; i < snitches.length; i++) {
+    if (snitches[i].resolved === false){
+      results.push(snitches[i]);
+    }
+  }
+  return results;
 }
+}
+
+
+// @Pipe({
+//   name: 'personalSnitchCount'
+// })
+// export class PersonalSnitchCountPipe implements PipeTransform {
+
+// transform(persSnitches: Snitch[]): Snitch[] {
+//   let results = [];
+
+//   for (let i = 0; i < snitches.length; i++) {
+//     if ( && persSnitches[i].resolved === false){
+//       results.push(persSnitches[i]);
+//     }
+//   }
+//   return results;
