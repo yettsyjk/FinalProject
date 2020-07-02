2a868a15ef8efdeeb17478bd9394ceccf41ecce5
Yettsy Knapp <yettsyknapp@SDMacBookPro3-2.local>
2020-07-02 09:46:57 -0600

    adding gradle

--- Snitch/src/main/java/com/skilldistillery/snitchapp/SnitchApplication.java
+++ Snitch/src/main/java/com/skilldistillery/snitchapp/SnitchApplication.java
@@ -2,13 +2,20 @@ package com.skilldistillery.snitchapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class SnitchApplication {
-
+public class SnitchApplication extends SpringBootServletInitializer {
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SnitchApplication.class);
+    }
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SnitchApplication.class, args);
 	}
